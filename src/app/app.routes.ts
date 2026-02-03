c19ed98c507f89c845d3480e5b2357f4fdae1534
Haadia Aamir <haamir@ssidecisions.com>
2026-02-03 19:10:46 +0500

    Frontend setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ResolveFn, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+const titleResolver: ResolveFn<string> = (route) => {
+    const id = route.params['id'];
+    return id ? `Project #${id}` : 'Project';
+};
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: titleResolver,
+    },
+    // {
+    //     // path: 'project',
+    //     // // component: Project #eager loading
+    //     // loadComponent: () => import('./components/project/project').then(m => m.ProjectComponent),  // lazy loading        title: 'Projects'
+    //     // title: titleResolver,
+    // },
+];
