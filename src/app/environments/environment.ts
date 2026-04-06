ba753192b707b096d7f23f827df82871b1a238df
Haadia Aamir <haadia.aamir@gmail.com>
2026-04-06 16:35:31 +0500

    Remove environment.ts from repo

--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://lopsggvdgqsporxemgbo.supabase.co',
-    supabaseAnonKey: 'sb_publishable_CGQX5aP_LCqeFGGz35gzKQ_9TTEOJCI',
-};
